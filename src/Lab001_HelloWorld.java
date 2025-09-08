36dc7c6caed3b56e636368d3e42cab8efe3fd22a
Aditya Prakash <adissm14@gmail.com>
2025-09-08 21:44:52 +0530

    1st code of Java

---
+++ src/Lab001_HelloWorld.java
@@ -0,0 +1,8 @@
+public class Lab001_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("ADITYA PRAKASH");
+        System.out.println("ADITYA PRAKASH");
+        System.out.println("ADITYA PRAKASH");
+        System.out.println("ADITYA PRAKASH");
+    }
+}
