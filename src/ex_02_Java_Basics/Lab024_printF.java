25f88f19d6c3898bd87c01e2158b808786949624
Aditya Prakash <adissm14@gmail.com>
2025-12-21 08:52:55 +0530

    New codes

---
+++ src/ex_02_Java_Basics/Lab024_printF.java
@@ -0,0 +1,20 @@
+package ex_02_Java_Basics;
+
+import java.util.Arrays;
+
+public class Lab024_printF {
+    public static void main(String[] args) {
+        int a = 10;
+        int b =20;
+        System.out.println(a);
+        System.out.println(b);
+
+        System.out.print(a);
+        System.out.print(b);
+        System.out.println();
+
+        System.out.printf("Value of a is %d",a);
+        System.out.println();
+        System.out.printf("Value of b is %d",b);
+    }
+}
