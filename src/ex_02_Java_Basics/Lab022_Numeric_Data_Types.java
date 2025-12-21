17934e7adc5d758b500a1d4c5b4737073fac2662
Aditya Prakash <adissm14@gmail.com>
2025-12-21 08:30:48 +0530

    New codes

---
+++ src/ex_02_Java_Basics/Lab022_Numeric_Data_Types.java
@@ -0,0 +1,19 @@
+package ex_02_Java_Basics;
+
+public class Lab022_Numeric_Data_Types {
+    public static void main(String[] args) {
+        byte b = 10;
+        short s =  10;
+        int i = 10;
+        char c = 'A';
+        long phone = 8757629933L;
+        long phone1 = 8757629933l;
+        float f = 3.14f;
+        float f1 = 3.14f;
+        double d = 3.125468;
+
+        System.out.print(phone1);
+
+
+    }
+}
