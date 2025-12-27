2579f742cfb3b8c50b8e6dd554ba58fbd83c91d9
Aditya Prakash <adissm14@gmail.com>
2025-12-27 11:59:30 +0530

    New codes

---
+++ src/ex_04_Operators/Lab040_Relational_Operator.java
@@ -0,0 +1,16 @@
+package ex_04_Operators;
+
+public class Lab040_Relational_Operator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 30;
+        boolean c  = a>b;
+        System.out.println(c);
+        System.out.println(a>b);
+
+        int age_mamtha = 33;
+        int age_adi = 34;
+        boolean result = age_adi >= age_mamtha;
+        System.out.println(result);
+    }
+}
---
+++ src/ex_04_Operators/Lab041_Logical_Operator.java
@@ -0,0 +1,17 @@
+package ex_04_Operators;
+
+public class Lab041_Logical_Operator {
+    public static void main(String[] args) {
+        boolean a = true;
+        System.out.println(!a);
+
+        boolean b = false;
+        System.out.println(!!b);
+
+        boolean c = true || false;
+        System.out.println(c);
+
+        boolean c1 = false && true;
+        System.out.println(c1);
+    }
+}
